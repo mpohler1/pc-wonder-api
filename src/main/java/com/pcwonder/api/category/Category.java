5d611af90a688053b5711784e0471945a7cdcf0c
mpohler1 <masonpohler@gmail.com>
2020-04-13 22:30:43 -0500

    Added list of manufacturers associated to a category within the Category entity. Changed tests to reflect the new Category entity

--- src/main/java/com/pcwonder/api/category/Category.java
+++ src/main/java/com/pcwonder/api/category/Category.java
@@ -1,17 +1,27 @@
 package com.pcwonder.api.category;
 
+import com.pcwonder.api.manufacturer.Manufacturer;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @Entity
 public class Category {
+
     @Id
     @GeneratedValue
     private long id;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.DETACH)
+    @JoinTable(
+            name = "category_manufacturer",
+            joinColumns = {@JoinColumn(name = "category_id", referencedColumnName = "id", nullable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "manufacturer_id", referencedColumnName = "id", nullable = false)}
+    )
+    @OrderBy("name ASC")
+    private List<Manufacturer> manufacturers;
 
     private String name;
 }
